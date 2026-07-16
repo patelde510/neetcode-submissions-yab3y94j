d32316ed60bfeedf3fcc012f6083e6e87c506801
patelde510 <94941963+patelde510@users.noreply.github.com>
2026-07-15 21:35:31 -0400

    Add: course-schedule - submission-11

---
+++ Data Structures & Algorithms/course-schedule/submission-11.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+
+        for crs, pre in prerequisites:
+            graph[crs].append(pre)
+        
+        visited = set()
+        def isCycle(crs):
+            if crs in visited:
+                return True
+
+            if graph[crs] == []:
+                return False
+            
+            visited.add(crs)
+
+            for pre in graph[crs]:
+                if isCycle(pre):
+                    return True
+
+            visited.remove(crs)
+        
+        for i in range(numCourses):
+            if isCycle(i):
+                return False
+
+        return True
